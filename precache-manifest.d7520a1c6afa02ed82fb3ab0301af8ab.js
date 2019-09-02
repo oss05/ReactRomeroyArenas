3df0005e416ac0b9ca5f1cc36455963fdc4a4984
Luis Osvaldo <aldolp40@gmail.com>
2019-09-02 11:43:55 -0500

    Updates

---
+++ precache-manifest.d7520a1c6afa02ed82fb3ab0301af8ab.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01ea7f0912c155d8ee5dc442f454518f",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "ff262bb264276db1a906",
+    "url": "/repository-name/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "e492ff2942ba7ea54075",
+    "url": "/repository-name/static/css/main.c2945c0a.chunk.css"
+  },
+  {
+    "revision": "ff262bb264276db1a906",
+    "url": "/repository-name/static/js/2.8fae2601.chunk.js"
+  },
+  {
+    "revision": "e492ff2942ba7ea54075",
+    "url": "/repository-name/static/js/main.3d02300c.chunk.js"
+  },
+  {
+    "revision": "6e2fc4b7d77a9c53df47",
+    "url": "/repository-name/static/js/runtime~main.f7634681.js"
+  },
+  {
+    "revision": "3dc1f419090f2af372870763d50b1f1c",
+    "url": "/repository-name/static/media/10.3dc1f419.png"
+  },
+  {
+    "revision": "660f9e430e1831e13524d9b91787cb6e",
+    "url": "/repository-name/static/media/11.660f9e43.png"
+  },
+  {
+    "revision": "2da2f98921924850db9caebab179729f",
+    "url": "/repository-name/static/media/3.2da2f989.png"
+  },
+  {
+    "revision": "f00ecb8b878f73c96e9d6e09c17c7a9e",
+    "url": "/repository-name/static/media/4.f00ecb8b.png"
+  },
+  {
+    "revision": "e311b940ec8d5fb712433713304e4bbc",
+    "url": "/repository-name/static/media/5.e311b940.png"
+  },
+  {
+    "revision": "9b2eab24fe6a4adfbf4ae3e35f7b2d9f",
+    "url": "/repository-name/static/media/7.9b2eab24.png"
+  },
+  {
+    "revision": "9476b1de897405fb8d6db59560521f1c",
+    "url": "/repository-name/static/media/8.9476b1de.png"
+  },
+  {
+    "revision": "00813ac36e655b32fbdd8c2f528ba163",
+    "url": "/repository-name/static/media/9.00813ac3.png"
+  },
+  {
+    "revision": "602852fc1c888623eb3ae722d528afe0",
+    "url": "/repository-name/static/media/aServicio.602852fc.jpg"
+  },
+  {
+    "revision": "2ae60c489f3b0db1295a3964dfd76c67",
+    "url": "/repository-name/static/media/boletines.2ae60c48.png"
+  },
+  {
+    "revision": "e8c0c8db6041c2767ab5cb6d8ee49d37",
+    "url": "/repository-name/static/media/boletines2017.e8c0c8db.png"
+  },
+  {
+    "revision": "b8c182f880fd9f073ffc7fcb387b1311",
+    "url": "/repository-name/static/media/boletines2018.b8c182f8.png"
+  },
+  {
+    "revision": "984e353d9143356732bcc319a8959458",
+    "url": "/repository-name/static/media/boletines2019.984e353d.png"
+  },
+  {
+    "revision": "7a8c988412ed6d9f9a96af7f18899171",
+    "url": "/repository-name/static/media/boletines_res.7a8c9884.png"
+  },
+  {
+    "revision": "1750807dbc75017cbf476441b79e0fe1",
+    "url": "/repository-name/static/media/brochure.1750807d.pdf"
+  },
+  {
+    "revision": "5fcfd6689ca29991c8555f501c61a9d9",
+    "url": "/repository-name/static/media/portada.5fcfd668.jpg"
+  },
+  {
+    "revision": "b0f4961f387c533d94e68ad40d93b03f",
+    "url": "/repository-name/static/media/portada_res.b0f4961f.png"
+  }
+]);
